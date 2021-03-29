515cf5b3ea860c7f6d5d3c9b8a05fb062e1dcd42
Nguyen Tran <48101680+tgm043@users.noreply.github.com>
2021-03-29 16:32:58 -0600

    - Writes to debug to track progress

--- Assets/Components/WorldSceneManager.cs
+++ Assets/Components/WorldSceneManager.cs
@@ -50,6 +50,7 @@ namespace Antymology.Terrain
                         }
                         
                         epoch++;
+                        Debug.Log(epoch);
                     }
                     
                     currentTime = 0;
